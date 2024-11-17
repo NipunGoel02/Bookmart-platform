58908e83f4944ce5e75a62c44c6aa419cd40e067
nipun <nipungoel15@gmail.com>
2024-11-17 11:31:44 +0530

    hello

--- main.js
+++ main.js
@@ -38,6 +38,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
 app.use(session({
     store: MongoStore.create({
